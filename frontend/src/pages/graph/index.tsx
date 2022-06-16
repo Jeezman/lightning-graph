34850deddc515f41c5e445d603e28f64edee91dc
Jeezman <tobiadeyemi91@gmail.com>
2022-06-16 13:21:08 +0100

    add graph screen

---
+++ frontend/src/pages/graph/index.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import { useSocket } from '../../hooks/UseSocket';
+import { useApi } from '../../hooks/UseApi';
+import { Lnd } from '../../services/ApiTypes';
+import { Graph } from './components/Graph';
+
+export const GraphScene = () => {
+  const api = useApi();
+  const graphRef = useRef<Graph>(null);
+
+  useEffect(() => {
+    api.fetchGraph().then((result) => {
+      console.log(result);
+      if(graphRef.current) graphRef.current.createGraph(result);
+    });
+  }, []);
+
+  useSocket('graph', (update: Lnd.GraphUpdate) => {
+     if(graphRef.current) graphRef.current.updateGraph(update);
+  });
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row h-100">
+        <div className="col h-100">{<Graph ref={graphRef} />}</div>
+      </div>
+    </div>
+  );
+};
